da66e9ee54816cdbb35b8aab3fddaf162c3f1cbb
Usuario <vicen.ll.d@gmail.com>
2023-02-11 20:41:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/incivismebueno/ui/home/HomeFragment.java
@@ -0,0 +1,55 @@
+package com.example.incivismebueno.ui.home;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.incivismebueno.SharedViewModel;
+
+import com.example.incivismebueno.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        SharedViewModel sharedViewModel = new ViewModelProvider(getActivity()).get(SharedViewModel.class);
+
+        SharedViewModel.getCurrentAddress().observe(getViewLifecycleOwner(), address -> {
+            binding.localitzacio.setText(String.format(
+                    "Direcció: %1$s \n Hora: %2$tr",
+                    address, System.currentTimeMillis()));
+        });
+        sharedViewModel.getButtonText().observe(getViewLifecycleOwner(), s -> binding.buttonlocation.setText(s));
+        sharedViewModel.getProgressBar().observe(getViewLifecycleOwner(), visible -> {
+            if (visible)
+                binding.progressBar.setVisibility(ProgressBar.VISIBLE);
+            else
+                binding.progressBar.setVisibility(ProgressBar.INVISIBLE);
+        });
+
+        binding.buttonlocation.setOnClickListener(view -> {
+            Log.d("DEBUG", "Clicked Get Location");
+            sharedViewModel.switchTrackingLocation();
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
